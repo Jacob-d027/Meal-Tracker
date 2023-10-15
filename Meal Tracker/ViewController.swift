6b5edd30a25cfaed05af5df3213430fa8d534f1e
Jacob Davis <jacobdofficial@gmail.com>
2023-10-14 22:21:42 -0600

    Initial Commit

---
+++ Meal Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meal Tracker
+//
+//  Created by Jacob Davis on 10/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
